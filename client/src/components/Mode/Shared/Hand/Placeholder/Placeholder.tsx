033a697eff19ac058a1e085d3b1c557836ba74d6
Vasyl Sunak <vasyl.sunak@lembergsolutions.com>
2023-05-09 19:04:22 +0300

    Minor style adjustments

--- client/src/components/Mode/Shared/Hand/Placeholder/Placeholder.tsx
+++ client/src/components/Mode/Shared/Hand/Placeholder/Placeholder.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { FC } from "react";
 import { Hand } from "@components/Mode/Shared/Hand/Hand";
 
@@ -5,16 +6,20 @@ interface Props {
   player: number;
 }
 
-export const Placeholder: FC<Props> = ({ player }) => {
-  return (
-    <div className="placeholder">
-      <Hand player={`Player ${player}`}>
-        {new Array(5).fill(null).map((_, index) => (
-          <div key={index} className="placeholder__wrapper">
-            <div className="placeholder__cube"></div>
-          </div>
-        ))}
-      </Hand>
-    </div>
-  );
-};
+export const Placeholder: FC<Props> = ({ player }) => (
+  <div className="placeholder">
+    <Hand player={`Player ${player}`}>
+      {new Array(5).fill(null).map((_, index) => (
+        <div key={index} className="placeholder__wrapper">
+          <Image
+            width={100}
+            height={100}
+            alt="grunge-square-filled"
+            className="placeholder__cube"
+            src="/images/grunge-square-filled.png"
+          />
+        </div>
+      ))}
+    </Hand>
+  </div>
+);
