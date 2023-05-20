2dcb4d12b349efe56316f34725056dec1a43cc80
Vasyl Sunak <vasyl.sunak@lembergsolutions.com>
2023-05-21 00:48:10 +0300

    Added confirmation before leave started game

--- client/src/components/Shared/Navigator/Navigator.tsx
+++ client/src/components/Shared/Navigator/Navigator.tsx
@@ -1,28 +1,37 @@
-import Link from "next/link";
 import Image from "next/image";
 import React, { FC } from "react";
+import { useRouter } from "next/router";
 import { usePortal } from "@utils/hooks/usePortal";
 import { useCursor } from "@utils/contexts/CursorProvider";
 
 interface Props {
   url?: string;
+  onNavigate?: () => void;
 }
 
-export const Navigator: FC<Props> = ({ url }) => {
+export const Navigator: FC<Props> = ({ url, onNavigate }) => {
   const Cursor = useCursor();
   const portal = usePortal();
+  const { push } = useRouter();
+
+  const handleNavigate = () => {
+    if (onNavigate) {
+      onNavigate();
+    } else {
+      push(url || "/");
+    }
+  };
 
   return portal(
-    <Link href={url || "/"}>
-      <Cursor hint="Back" id="navigator-back">
-        <Image
-          width={50}
-          height={50}
-          alt="arrow-back"
-          className="navigator"
-          src="/images/arrow-back.png"
-        />
-      </Cursor>
-    </Link>
+    <Cursor hint="Back" id="navigator-back">
+      <Image
+        width={50}
+        height={50}
+        alt="arrow-back"
+        className="navigator"
+        onClick={handleNavigate}
+        src="/images/arrow-back.png"
+      />
+    </Cursor>
   );
 };
