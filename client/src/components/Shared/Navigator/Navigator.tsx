5b7b5fd8dde68d625765fa4abb4b05d5fd05425f
Vasyl Sunak <vasyl.sunak@lembergsolutions.com>
2023-05-30 23:55:16 +0300

    Added FadeIn to Navigator

--- client/src/components/Shared/Navigator/Navigator.tsx
+++ client/src/components/Shared/Navigator/Navigator.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { FC } from "react";
 import { useRouter } from "next/router";
 import { usePortal } from "@utils/hooks/usePortal";
+import { FadeIn } from "@components/Shared/FadeIn/FadeIn";
 import { useCursor } from "@utils/contexts/CursorProvider";
 
 interface Props {
@@ -23,16 +24,18 @@ export const Navigator: FC<Props> = ({ url, onNavigate }) => {
   };
 
   return portal(
-    <Cursor hint="Back" id="navigator-back">
-      <div className="navigator" onClick={handleNavigate}>
-        <Image
-          width={50}
-          height={50}
-          alt="arrow-back"
-          className="navigator__image"
-          src="/images/arrow-back.png"
-        />
-      </div>
-    </Cursor>
+    <FadeIn>
+      <Cursor hint="Back" id="navigator-back">
+        <div className="navigator" onClick={handleNavigate}>
+          <Image
+            width={50}
+            height={50}
+            alt="arrow-back"
+            className="navigator__image"
+            src="/images/arrow-back.png"
+          />
+        </div>
+      </Cursor>
+    </FadeIn>
   );
 };
