9bc66d28692aa8a831f9f1ea93401975a10caca9
Vasyl Sunak <vasyl.sunak@lembergsolutions.com>
2023-06-04 13:38:30 +0300

    Fixing cursor fadein and out strange behaviour

--- client/src/components/Shared/Layout/Menu/Amulet/Amulet.tsx
+++ client/src/components/Shared/Layout/Menu/Amulet/Amulet.tsx
@@ -10,16 +10,29 @@ export const Amulet: FC<Props> = ({ toggleOpen }) => {
   const Cursor = useCursor();
 
   return (
-    <Cursor id="menu" hint="Menu" position="bottom-left">
-      <Image
-        priority
-        width={100}
-        height={55}
-        alt="amulet"
-        className="amulet"
-        onClick={toggleOpen}
-        src="/images/amulet.png"
-      />
-    </Cursor>
+    <>
+      <Cursor id="menu" hint="Menu" position="bottom-left">
+        <Image
+          priority
+          width={100}
+          height={55}
+          alt="amulet"
+          className="amulet"
+          onClick={toggleOpen}
+          src="/images/amulet.png"
+        />
+      </Cursor>
+      <svg style={{ opacity: 0, position: "absolute", zIndex: -99999 }}>
+        <circle>
+          <animate
+            to="1"
+            dur="3s"
+            from="0"
+            attributeName="r"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </svg>
+    </>
   );
 };
